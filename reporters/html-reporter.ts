bc5f2be67b1d6dfc382b7faca2728a830accd920
qa-riccardo <riccardo.vava@gmail.com>
2026-05-10 17:50:46 +0200

    Update html-reporter.ts

--- reporters/html-reporter.ts
+++ reporters/html-reporter.ts
@@ -1271,6 +1271,9 @@ function buildMultiRunHTML(runs: RunEntry[]): string {
 <head>
   <meta charset="UTF-8"/>
   <meta name="viewport" content="width=device-width,initial-scale=1"/>
+  <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate"/>
+  <meta http-equiv="Pragma" content="no-cache"/>
+  <meta http-equiv="Expires" content="0"/>
   <title>Foleon Quality Report</title>
   <style>${CSS}</style>
 </head>
